eef543ccb09c33a0741463b0b0e7d61d075a321b
Varadi, Gyorgy <gvaradi@ebay.com>
2018-06-25 08:19:43 +0200

    Initial Commit

---
+++ RcServoControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RcServoControl
+//
+//  Created by Varadi, Gyorgy on 25/06/2018.
+//  Copyright © 2018 Varadi, Gyorgy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
